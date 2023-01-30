5a17bac2f042bfaaa5457d18ca0ea84638c309a0
Lina <lina_holmlund@hotmail.com>
2023-01-30 10:44:30 +0100

    editor.css

--- includes/setup.php
+++ includes/setup.php
@@ -2,10 +2,11 @@
 
 function t_setup_theme() {
   add_theme_support('editor-styles');
-  
+
   add_editor_style([
     'https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;1,400&display=swap',
     'assets/bootstrap-icons/bootstrap-icons.css',
-    'assets/public/index.css'
+    'assets/public/index.css',
+    'assets/editor.css'
   ]);
 }
